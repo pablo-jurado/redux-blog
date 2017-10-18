038e033319bc4598a146c46077158337618f79b4
Pablo Jurado <pjurado@overnitecbt.com>
2017-10-18 15:46:14 -0500

    change redux form to a class component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,39 +1,51 @@
 import React, { Component } from 'react'
-import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { createPost } from '../actions/index'
 
 
-  // outside your render() method
-  const renderField = (field) => (
-    <div className="input-row">
-      <input {...field.input} type="text"/>
-      {field.meta.touched && field.meta.error && 
-      <span className="error">{field.meta.error}</span>}
-    </div>
-  )
 
 class PostsNew extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state= {
+      title: '',
+      categories: '',
+      content: ''
+    }
+
+    this.handleForm = this.handleForm.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    
+  }
+
+  handleForm (event) {
+    const value = event.target.value
+    const name = event.target.name
+    this.setState( { [name]: value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.createPost(this.state)
+  }
 
   render () {
-    //  const handleSubmit = this.props.handleSubmit
-    // this is the same
-    const { handleSubmit } = this.props
-    // const { fields: { title, categories, content } } = this.props
-    console.log(this.props)
     return (
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={this.handleSubmit}>
         <h3>New Post</h3>
         <div className='form-group'>
           <label>Title</label>
-          <Field component={renderField} name='title' type='text' className='form-control' />
+          <input name='title' type='text' className='form-control' onChange={this.handleForm} value={this.state.title} />
         </div>
         <div className='form-group'>
           <label>categories</label>
-          <Field component='input' name='categories' type='text' className='form-control' />
+          <input name='categories' type='text' className='form-control' onChange={this.handleForm} value={this.state.categories} />
           </div>
         <div className='form-group'>
           <label>content</label>
-          <Field component='input' name='content' type='text-area' className='form-control' />
+          <textarea name='content' type='text-area' className='form-control' onChange={this.handleForm} value={this.state.content} />
           </div> 
         <button type='submit' className='btn btn-primary'>submit</button>
       </form>   
@@ -41,7 +53,8 @@ class PostsNew extends Component {
   }
 }
 
-export default reduxForm ({
-  form: 'PostsNew'
-  // fields: ['title', 'categories', 'content']
-}, null, { createPost })(PostsNew)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators( { createPost }, dispatch )
+}
+
+export default connect(null, mapDispatchToProps)(PostsNew)
